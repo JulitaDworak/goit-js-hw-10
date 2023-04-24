92179c64d153033f76f1660ad8409dc177e28bd8
JulitaDworak <julita.dworak@gmail.com>
2023-04-24 10:42:46 +0200

    hw10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,13 @@
 const API_URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = (name) => {
-   return  fetch(`${API_URL}/${name}?fields=name,capital,population, language
+   return  fetch(`${API_URL}/${name}?fields=name,capital,population,flags,languages
    `)
-    .then((res) => res.json());
+    .then((res) => res.json())
+
+    // .catch (error =>  {
+    //     Notiflix.Notify.failure('Oops, there is no country with that name');
+    // })    
+    
 }
 
--- src/index.js
+++ src/index.js
@@ -5,114 +5,72 @@ import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
 const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
 const searchBox = document.getElementById('search-box')
 
 const DEBOUNCE_DELAY = 300;
 
 searchBox.addEventListener('input', debounce(async (e) => {
-console.log(`hello today`);
-    const countries = await fetchCountries((e.target.value))
-},DEBOUNCE_DELAY)
+
+// const countryValue = e.target.value
+// const countryName = countryValue.trim()
+const countryName = e.target.value
+
+
+const countries = await fetchCountries((countryName))
+
+if(countryName === "") {
+    countryList.innerHTML="";
+    countryInfo.innerHTML="";
+    return
+}
+
+if (countries.length >10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+else (countries.length >= 2 && countries.length <=10) 
+{   
+     countryList.innerHTML = countries.map((country) => 
+`<li> <img height="24" src="${country.flags.png}" />${country.name.common}</li>`
+)
+     .join("");
+
+
+
+    //  countryList.addEventListener('click', (e)=> {
+    //     console.log(`object`);
+    
+    //     countryList.innerHTML = countries.map((country) => 
+    //     `<li> <img height="24" src="${country.flags.png}" />${country.name.common}</li>`
+    //     )
+    //          .join("");
+    //     countryInfo.innerHTML = 
+    //     `
+    //  <p>Capital: ${countries[0].capital}</p>
+    //  <p>Population: ${countries[0].population}</p>
+    //  <p>Languages: ${Object.values([0].languages).join(',')}</p>
+    // `;
+    // })
+    
+    // console.log(countries.indexOf());
+
+
+    };
+
+
+if (countries.length === 1) {
+    countryInfo.innerHTML = 
+    `
+ <p>Capital: ${countries[0].capital}</p>
+ <p>Population: ${countries[0].population}</p>
+ <p>Languages: ${Object.values(countries[0].languages).join(',')}</p>
+`;
+ }
+ if (countries.length === null ){
+    `
+ <p> Nie ma takiego czegoś </p>
+`}
+ }, DEBOUNCE_DELAY)
 )
 
-// const API_URL = "https://restcountries.com/v3.1/all"
-// console.log(API_URL);
-// fetch(API_URL)
-// .then((response) => response.json())
-// .then((data)=> {
-// countryData = data[0]
-// console.log(countryData);
-// const countryInfo = API_URL.map((country) => countryData.name.common)
-// console.log(countryInfo);
-// }
-
-
-
-
-
-// countryItem.addEventListener('input', debounce((e) => {
-//     console.log(`hello`)
-// let text = countryItem.value
-// console.log(text );
-// let finalURL = `https://restcountries.com/v3.1/name/${text}?`
-// console.log(finalURL);
-
-
-
-// fetch(finalURL)
-// .then((response) => response.json())
-// .then((data)=> {
-// countryData = data[0]
-// console.log(countryData);
-
-// let country = countryData.name.common
-
-// // const listOfCountries = country.filter((country) => country.toLowerCase().includes(text.toLowerCase()))
-
-
-// console.log(country.split(""));
-
-// // const filterCountry = country.filter((name) => name.toLowerCase().includes(text.toLowerCase()))
-
-// console.log(country);
-
-// // countryList.textContent = `<li>${filterCountry}<li>`
-// if(text.includes(country.split(''))) {
-// flag.src = countryData.flags.svg;
-// name.innerHTML = countryData.name.common;
-// capital.innerHTML = countryData.capital;
-// continent.innerHTML = countryData.continents;
-// population.innerHTML = countryData.population;
-// currency.innerHTML = countryData.currencies[Object.keys(countryData.currencies)].name;
-// currencyShort.innerHTML = Object.keys(countryData.currencies)[0];
-// language.innerHTML = Object.values(countryData.languages).toString().split(',').join(',');
-
-// // return console.log(`to był dobry pomysł`)
-//     // `<li>${countryData.name.common}</li>`
-// }
-// else {
-//    console.log(`tragedia`);
-// }
-
-
-
-// })
-// // const country = 
-
-// // const listOfCountries = 
-
-// },DEBOUNCE_DELAY)
-// )
-
-
-
-// searchBtn.addEventListener('click', ()=> {
-// let countryName = countryItem.value;
-// let finalURL = `https://restcountries.com/v3.1/name/${countryName}?`
-// console.log(finalURL);
-
-// Notiflix.Report.warning('Uwaga', 'Znaleźliśmy szykany kraj', 'sprawdź wyniki wyszukowania');
-
-// fetch(finalURL)
-// .then((response) => response.json())
-// .then((data)=> {
-// countryData = data[0]
-// console.log(countryData);
-// flag.src = countryData.flags.svg;
-// name.innerHTML = countryData.name.common;
-// capital.innerHTML = countryData.capital;
-// continent.innerHTML = countryData.continents;
-// population.innerHTML = countryData.population;
-// currency.innerHTML = countryData.currencies[Object.keys(countryData.currencies)].name;
-// currencyShort.innerHTML = Object.keys(countryData.currencies)[0];
-// language.innerHTML = Object.values(countryData.languages).toString().split(',').join(',');
-// })
-// })
-
-//  `<li>
-//  <h2>Country: ${item.name.common}</h2>
-//  <h2>Capital: ${item.name.common}</h2>
-//  <h2>Population: ${item.name.common}</h2>
-//  <h2>Flags: ${item.name.common}</h2>
-//  <h2>Languages: ${item.name.common}</h2>
-//  </li>`
